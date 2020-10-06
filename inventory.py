58e996310401b51eae4a5c542904ad31a11233fb
Doug <70054506+smykad@users.noreply.github.com>
2020-10-06 08:53:46 -0400

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -5,37 +5,59 @@
 # * Date Revised: 10.6.2020     *
 # *******************************
 
-
+# Function for adding a new item to the inventory
 def addNewItem(info):
     global inventory
     inventory[info[0]] = {'Quantity': info[1], 'Price': info[2], 'Description': info[3]}
 
-
+# Function for getting information from user
 def newItem():
     print('item: ', end='')
-    v1 = input()
+    v1 = input().lower()
     print('Quantity: ', end='')
-    v2 = input()
+    v2 = input().lower()
     print('Price: ', end='')
-    v3 = input()
+    v3 = input().lower()
     print('Description: ', end='')
-    v4 = input()
+    v4 = input().lower()
     addNewItem([v1, v2, v3, v4 ])
 
-
+# Function for asking user if they would like to add an item
 def askUser():
     addItem = True
     while addItem is True:
         print('Would you like to add an item? ', end='')
-        userResponse = input()
+        userResponse = input().lower()
         if userResponse == 'yes' or userResponse == 'y':
             newItem()
         else:
             addItem = False
 
+# Function to search for an item
+def itemSearch():
+    global inventory
+    print('Please enter the item you are searching for: ', end='')
+    userResponse = input().lower()
+    if userResponse in inventory:
+        print(inventory[userResponse])
+    else:
+        print('Item not in inventory')
 
+# Function to ask user if they want to search for an item
+def askSearch():
+    search = True
+    while search is True:
+        print('Would you like to search for an item in the inventory? ', end='')
+        userResponse = input().lower()
+        if userResponse == 'yes' or userResponse == 'y':
+            itemSearch()
+        else:
+            search = False
+
+# Initialize dictionary
 inventory = {}
 
 askUser()
+askSearch()
 
 print(inventory)
