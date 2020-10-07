f201f0800c3890f73aecf10cf72248123190702e
Doug <70054506+smykad@users.noreply.github.com>
2020-10-07 09:32:11 -0400

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -2,16 +2,17 @@
 # * Author: Doug Smyka          *
 # * Application: Inventory      *
 # * Date Created: 10.6.2020     *
-# * Date Revised: 10.6.2020     *
+# * Date Revised: 10.7.2020     *
 # *******************************
 
 # Function for adding a new item to the inventory
-def addNewItem(info):
+def add_new_item(info):
     global inventory
     inventory[info[0]] = {'Quantity': info[1], 'Price': info[2], 'Description': info[3]}
 
+
 # Function for getting information from user
-def newItem():
+def new_item():
     global itemList
     print('item: ', end='')
     v1 = input().lower()
@@ -22,59 +23,69 @@ def newItem():
     v3 = input().lower()
     print('Description: ', end='')
     v4 = input().lower()
-    addNewItem([v1, v2, v3, v4 ])
+    add_new_item([v1, v2, v3, v4])
+
 
 # Function for asking user if they would like to add an item
-def askUser():
-    addItem = True
-    while addItem is True:
+def ask_user():
+    add_item = True
+    while add_item is True:
         print('Would you like to add an item? ', end='')
-        userResponse = input().lower()
-        if userResponse == 'yes' or userResponse == 'y':
-            newItem()
+        user_response = input().lower()
+        if user_response == 'yes' or user_response == 'y':
+            new_item()
         else:
-            addItem = False
+            add_item = False
 
 # Function to search for an item
-def itemSearch():
+def item_search():
     global inventory
     print('Please enter the item you are searching for: ', end='')
-    userResponse = input().lower()
-    if userResponse in inventory:
-        print('Quantity: %(Quantity)s Price: %(Price)s Description: %(Description)s' % (inventory[userResponse]))
+    user_response = input().lower()
+    if user_response in inventory:
+        print('Quantity: %(Quantity)s Price: %(Price)s Description: %(Description)s' % (inventory[user_response]))
     else:
         print('Item not in inventory')
 
+
 # Function to ask user if they want to search for an item
-def askSearch():
+def ask_search():
     search = True
     while search is True:
         print('Would you like to search for an item in the inventory? ', end='')
-        userResponse = input().lower()
-        if userResponse == 'yes' or userResponse == 'y':
-            itemSearch()
+        user_response = input().lower()
+        if user_response == 'yes' or user_response == 'y':
+            item_search()
         else:
             search = False
 
 
 # Function for printing table
-def printTable():
+def print_table():
     global inventory
     global itemList
     j = 0
+    print('\n*******************   Inventory   **********************\n')
     for i in inventory:
         item = itemList[j]
         j += 1
-        print('%s: ' % item)
+        print('[%s] ' % item, end='')
         print('Quantity: %(Quantity)s Price: %(Price)s Description: %(Description)s' % (inventory[i]))
+
+    print('\n********************************************************\n')
+
+
+def main_function():
+    ask_user()
+    ask_search()
+    print_table()
+
 
 # Initialization
 inventory = {}
 itemList = []
 
 # Main
-askUser()
-askSearch()
-printTable()
+main_function()
 
-#EOF
+# EOF
